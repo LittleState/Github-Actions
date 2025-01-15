e3e83f3ebc91b4bb41ea972fd77a79830cbfdc29
小邦 <lvliangzebang@gmail.com>
2025-01-15 16:16:04 +0800

    Update git_pull_work.sh

--- scripts/git_pull_work.sh
+++ scripts/git_pull_work.sh
@@ -8,16 +8,16 @@ NC='\033[0m'
 for dir in ./*/; do
     if [ -d "$dir/.git" ]; then
         echo -e "${YELLOW}Updating ${dir}${NC}"
-	    (
-		    cd "$dir" && git pull > /dev/null 2>&1
-		    if [ $? -eq 0 ]; then
-				echo -e "${GREEN}Update completed for ${dir}${NC}"
-			else
-				echo -e "${RED}Update Failed for ${dir}${NC}"
-			fi
-	    ) &
+        (
+            cd "$dir" && git pull > /dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                echo -e "${GREEN}Update completed for ${dir}${NC}"
+            else
+                echo -e "${RED}Update Failed for ${dir}${NC}"
+            fi
+        ) &
     else
-	    echo "${RED}${dir} is not a git repository${NC}"
+        echo "${RED}${dir} is not a git repository${NC}"
     fi
 done
 
